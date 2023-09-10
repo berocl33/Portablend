d61de939556bc30764cb22c39f450069d99c2a9f
B. rockLee <121263424+berocl33@users.noreply.github.com>
2023-09-09 23:51:57 -0700

    scripting.py

--- scripting.py
+++ scripting.py
@@ -2,7 +2,7 @@
 #which of our android-blender demo is.
 import bpy#(if from a threaded process use 
 _bpy=bpy#_bpy it is builtin to the binary)
-from code import CommandCompiler as con
+from code import InteractiveConsole as con
 from binascii import unhexlify as jim
 try:
   from urllib import unquote as skt
@@ -10,6 +10,12 @@ except:
   from httplib import unquote as skt
 spc=[a+a*2 for a in range(3,19,2)];
 
+class funcparl():
+ def __init__(self,ft):
+  self.ft=ft
+ def __getattr__(self,c):
+  return(lambda c,*a:functools.partial(
+getattr(self.ft,c)(*a) and self.ft or self.ft))
 
 def execqueued(this=None):
   if(this in _bpy.data.scenes):
@@ -26,13 +32,36 @@ def execqueued(this=None):
             )]][0];
     upm=params.keys();
   #fi
-  con.runsource(chr(10).join([q.decode() if(isinstance(q,bytes)) else q for q in queue]))
+  execTimer.con.runsource(chr(10).join([q.decode() if(isinstance(q,bytes)) else q for q in queue]))
   return([q for q in ([1.0],) for r,s in zip(q,upm)] if(execqueued.bms>0.1) else 1.0)
         
 #fed 
-
+class execTimer(bpy.types.Operator):
+ bl_idname="wm.exec_timer"
+ bl_label="exec timer"
+ rtdelay=3.0#line by line Slow
+ def modal(self,ctx={},event=None):
+   if(event.type=="TIMER"):
+     execqueued()
+     return {"RUNNING_MODAL"}
+   return {"PASS_THROUGH"}
+    
+ def execute(self,ctx={}):
+   if(not getattr(self,"con",None)):
+     self.con=InteractiveConsole()
+   #fi
+   ctx.window_manager.modal_handler_add(self)
+   setattr(this,[b and getattr(self,str(a[-1]+1),
+for a in ([1],) for b in (not hasattr(self,str(a[-1])) or a.append(a[-1]+1))][-1],
+ctx.window_manager.register_event_timer(self.rtdelay,ctx.window))      
+   return {"RUNNING_MODAL"}
 
 def register():
+  try:
+   bpy.util.register_class(execTimer)
+   bpy.ops.wm.exec_timer()
+  except(Exception):
+   execqueued.con._exc=sys.exc_info()
    cmdhash=repr(execqueue)[1:-10];
    cmdindexs=[repr(af)[1:-10] for af in bpy.app.handlers.scene_update]
    if cmdhash not in cmdindexs:
